3dff6f723f0a6f2e451f5a32186f91ec74ab302b
sangwon <sosimman01@naver.com>
2023-04-11 22:47:26 +0900

    프로그래머스 입문

---
+++ 프로그래머스 입문/1day/1일차/1일차/두수의 합.c
@@ -0,0 +1,20 @@
+﻿/*
+문제 설명
+정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+제한사항
+-50,000 ≤ num1 ≤ 50,000
+-50,000 ≤ num2 ≤ 50,000
+*/
+
+#include <stdio.h>
+#include <stdbool.h> 
+#include <stdlib.h> // 절대값을 구하는 abs 함수
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    if ((50000 >= abs(num1)) && (50000 >= abs(num2))) {
+        answer = num1 + num2;
+        return answer;
+    }
+}
